f00d1c35021ba2f23decca816a2f880c1628e583
rzins <laniakean.dev@gmail.com>
2026-08-05 14:44:34 +0200

    tsx icons

---
+++ public/plus.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+interface PlusIconProps {
+  size?: number;
+}
+
+const PlusIcon: React.FC<PlusIconProps> = ({ size = 40 }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+      />
+    </svg>
+  );
+};
+
+export default PlusIcon;
