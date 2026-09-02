6a82a513e332f2366ea7ad7f428b760c12960e70
rcastel <rcastel@c2r9s2.marseille.42.school>
2026-09-02 17:02:50 +0200

    real time messages fix

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,12 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
-let supabase: ReturnType<typeof createClient> | undefined;
+let supabase: ReturnType<typeof createClient> | null = null;
 
 export function getSupabaseBrowserClient() {
   if (!supabase) {
+    console.log('SUPABASE BROWSER CLIENT CREATED');
+
     supabase = createClient(
       process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+      process.env.NEXT_PUBLIC_SUPABASE_KEY!
     );
   }
 
