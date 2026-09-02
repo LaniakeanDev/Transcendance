034fa4c5828b7ac7b7c9e507a25b4076dcdf214c
rcastel <rcastel@c2r9s2.marseille.42.school>
2026-09-02 16:38:25 +0200

    real time messages fix

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,8 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
+let supabase: ReturnType<typeof createClient> | undefined;
+
 export function getSupabaseBrowserClient() {
-  return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_KEY!
-  );
+  if (!supabase) {
+    supabase = createClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    );
+  }
+
+  return supabase;
 }
